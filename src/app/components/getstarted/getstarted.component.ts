57b22da0a86ef22ad795a541706397658c920832
Cunn <concepcion3espinas@protonmail.com>
2024-07-12 14:05:48 +0200

    initpage done

--- src/app/components/getstarted/getstarted.component.ts
+++ src/app/components/getstarted/getstarted.component.ts
@@ -1,6 +1,6 @@
-import { AfterViewInit, Component, inject, Inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
-import {initTooltips } from 'flowbite';
-import { MatchcarouselComponent} from '../matchcarousel/matchcarousel.component';
+import { AfterViewInit, Component, HostListener, inject, Inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
+import { initTooltips } from 'flowbite';
+import { MatchcarouselComponent } from '../matchcarousel/matchcarousel.component';
 import { FlowbiteService } from '../../services/flowbite.service';
 import { Router } from '@angular/router';
 
@@ -12,14 +12,42 @@ import { Router } from '@angular/router';
   templateUrl: './getstarted.component.html',
   styleUrls: ['./getstarted.component.scss']
 })
-export class GetstartedComponent implements AfterViewInit, OnInit{
-  
+export class GetstartedComponent implements AfterViewInit, OnInit {
+
   private flowbitesvc = inject(FlowbiteService);
+  public tipsUnseen: string[] = ['Match', 'Linx', 'Cadena'];
+  public tipsToRead: string[] = ['Match'];
+  timeoutId: any;
 
-  constructor(private router : Router){}
+  constructor(private router: Router) { }
+
+  get shouldAnimate(): boolean {
+    return this.tipsUnseen.includes('Match') && this.tipsToRead.includes('Match');
+  }
   
+  mousOverReadTip(event: MouseEvent) {
+    if (this.tipsUnseen.length > 0) {
+      const divElement = event.target as HTMLElement;
+      let divid = divElement.id
 
-  goToMeeting(){
+      const index = this.tipsUnseen.findIndex(tip => tip === divid)
+
+      if (index !== -1) {
+        this.tipsUnseen.splice(index, 1)
+        this.tipsToRead = [];
+      }
+    }
+  }
+
+  mouseOutNextTip(event: MouseEvent) {
+    if (this.tipsUnseen.length > 0) {
+      this.tipsToRead = [];
+      clearTimeout(this.timeoutId);
+      this.timeoutId = setTimeout(() => this.tipsToRead.push(this.tipsUnseen[0]), 1000);
+    }
+  }
+
+  goToMeeting() {
     this.router.navigateByUrl('/Linx/matching')
   }
 
