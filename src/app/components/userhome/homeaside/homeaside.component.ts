e4190f390ccea8a03ff895d28faf70b322b680ab
Cunn <concepcion3espinas@protonmail.com>
2024-07-16 19:36:18 +0200

    generating user home components

---
+++ src/app/components/userhome/homeaside/homeaside.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homeaside',
+  standalone: true,
+  imports: [],
+  templateUrl: './homeaside.component.html',
+  styleUrl: './homeaside.component.scss'
+})
+export class HomeasideComponent {
+
+}
